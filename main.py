b79d32c2b17a8ca6265958036b79598d705bfced
akkaynt1369-boop <akkaynt1369@gmail.com>
2026-05-27 01:26:24 +0000

    main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ R2_ACCOUNT_ID = os.getenv("R2_ACCOUNT_ID")
 R2_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
 R2_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
 R2_BUCKET_NAME = "mystillfots"
-R2_PUBLIC_URL = "https://pub-509a306816994714a761c583f2788500.r2.dev"
+R2_PUBLIC_URL = os.getenv("R2_PUBLIC_URL", "https://pub-509a306816994714a761c583f2788500.r2.dev")
 
 album_buffer = defaultdict(lambda: {"photos": [], "caption": "", "timer": None})
 
